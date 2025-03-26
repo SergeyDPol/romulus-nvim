81510578593fe226283703745e490f5c10c9ce30
Sergey Polukhin <sergeydpol@gmail.com>
2025-03-26 19:20:07 +0300

    Refactor: Change grammar url

--- lua/romulus/init.lua
+++ lua/romulus/init.lua
@@ -4,7 +4,7 @@ local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
 parser_config.myparser = {
   install_info = {
-    url = "https://github.com/SergeyDPol/tree-sitter-romulus",
+    url = "SergeyDPol/tree-sitter-romulus",
     files = { "src/parser.c" },
     branch = "main",
     requires_generate_from_grammar = true,
